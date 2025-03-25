d2d2d590bc0cbe45567a479bf68c6d3ab14e39a2
Jorge Sanchez Lorente <jorgesanlor1221@gmail.com>
2025-03-25 16:15:05 +0100

    Añadir seminario 2

---
+++ Seminario2/dispositivo/src/dispositivo/interfaces/IDispositivo.java
@@ -0,0 +1,20 @@
+package dispositivo.interfaces;
+
+import java.util.Collection;
+
+public interface IDispositivo {
+	public String getId();
+	
+	public IDispositivo iniciar();
+	public IDispositivo detener();
+		
+	public IDispositivo addFuncion(IFuncion f);
+	public IFuncion getFuncion(String funcionId);
+	public Collection<IFuncion> getFunciones();
+
+	public Boolean getHabilitado();
+	public void habilitar();
+	public void deshabilitar();
+
+		
+}
